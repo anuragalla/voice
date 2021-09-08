e383dbd46d63c730804955e245c21dfdde226061
malleswararao <malli.babu@inventrix.co.in>
2021-09-08 18:31:11 +0530

    testing audio  recoding

--- android/src/main/java/com/wenkesj/voice/VoiceModule.java
+++ android/src/main/java/com/wenkesj/voice/VoiceModule.java
@@ -435,10 +435,12 @@ public class VoiceModule extends ReactContextBaseJavaModule implements Recogniti
     WritableMap event = Arguments.createMap();
     event.putArray("value", arr);
     event.putArray("confidence", confidenceArr);
-    if(this.myAudioFile != null) {
+    if(this.myAudioRecorder != null) {
       this.myAudioRecorder.stop();
-      event.putString("audioFilePath", this.myAudioFile.getAbsolutePath());
-      Log.d("ASR", "file is not empty and size : "+getFileSizeKiloBytes(this.myAudioFile)+" location :"+this.myAudioFile.getAbsolutePath()+" path :"+this.myAudioFile.getPath());
+      if(this.myAudioFile != null) {
+        event.putString("audioFilePath", this.myAudioFile.getAbsolutePath());
+        Log.d("ASR", "file is not empty and size : "+getFileSizeKiloBytes(this.myAudioFile)+" location :"+this.myAudioFile.getAbsolutePath()+" path :"+this.myAudioFile.getPath());
+      }
     } else {
       Log.d("ASR", "file is empty");
       event.putNull("audioFilePath");
